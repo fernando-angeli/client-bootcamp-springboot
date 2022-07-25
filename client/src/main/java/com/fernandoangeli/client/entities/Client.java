8f8a62664765d01907c5b4673d64d69dff936e1c
fernando-angeli <fernando_angeli@hotmail.com>
2022-07-24 22:58:52 -0300

    GET client (findById) and exceptions

--- client/src/main/java/com/fernandoangeli/client/entities/Client.java
+++ client/src/main/java/com/fernandoangeli/client/entities/Client.java
@@ -1,12 +1,16 @@
 package com.fernandoangeli.client.entities;
 
-import lombok.Data;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.Instant;
+import java.util.Objects;
 
 @Entity
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "tb_client")
 public class Client {
 
@@ -16,7 +20,20 @@ public class Client {
     private String name;
     private String cpf;
     private Double income;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant birthDate;
     private Integer children;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- client/src/main/java/com/fernandoangeli/client/services/ClientService.java
+++ client/src/main/java/com/fernandoangeli/client/services/ClientService.java
@@ -3,6 +3,7 @@ package com.fernandoangeli.client.services;
 import com.fernandoangeli.client.dto.ClientDTO;
 import com.fernandoangeli.client.entities.Client;
 import com.fernandoangeli.client.repositories.ClientRepository;
+import com.fernandoangeli.client.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,9 +16,14 @@ public class ClientService {
     @Autowired
     private ClientRepository repository;
 
-    @Transactional
+    @Transactional(readOnly = true)
     public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
         Page<Client> list = repository.findAll(pageRequest);
         return list.map(ClientDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        return new ClientDTO(repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Id "+ id + " não encontrado")));
     }
 }
